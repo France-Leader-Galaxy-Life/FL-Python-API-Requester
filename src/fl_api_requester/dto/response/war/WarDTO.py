0b4faf56aa96d78db3202f11ca4ea370b07e2789
Jordan <83838658+KinderPinGuiin@users.noreply.github.com>
2023-07-03 21:44:16 +0200

    feat: Added war start timestamp

--- src/fl_api_requester/dto/response/war/WarDTO.py
+++ src/fl_api_requester/dto/response/war/WarDTO.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 from ...DTO import DTO
 from ...response.alliance.AllianceNameDTO import AllianceNameDTO
 
@@ -13,3 +14,7 @@ class WarDTO(DTO):
     alliancePoints: int 
     opponentPoints: int 
     status: str
+    timestamp: int
+
+    def timestamp_to_datetime(self) -> datetime:
+        return datetime.fromtimestamp(float(self.timestamp) / 1000)
